b6e366a32c3b5a482275faf53156c6b3e47b54db
Alexkev <Alex.kmatheson@gmail.com>
2019-03-12 11:51:58 -0600

    Finished Add Desk

--- MegaDeskWebApp/Desk.cs
+++ MegaDeskWebApp/Desk.cs
@@ -5,11 +5,19 @@ using System.Web;
 
 namespace MegaDesk
 {
+    
     public class Desk
     {
         public decimal Width { get; set; }
         public decimal Depth { get; set; }
         public int NumDrawers { get; set; }
         public int MaterialId { get; set; }
+        public Desk(decimal width, decimal depth, int numDrawers, int materialId)
+        {
+            Width = width;
+            Depth = depth;
+            NumDrawers = numDrawers;
+            MaterialId = materialId;
+        }
     }
 }
